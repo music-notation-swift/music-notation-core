8f40401999cc852728078af7ffdb98c092e55f19
drumnkyle <kyledsherman@gmail.com>
2015-06-18 23:06:07 -0700

    Added intervals

--- MusicNotationKit/MusicNotationKit/Types.swift
+++ MusicNotationKit/MusicNotationKit/Types.swift
@@ -76,3 +76,60 @@ public enum Dynamics {
 	case Ff
 	case Fff
 }
+
+public enum Interval: Int {
+	case Unison = 0
+	case Min2
+	case Maj2
+	case Min3
+	case Maj3
+	case Perfect4
+	case Aug4
+	case Perfect5
+	case Min6
+	case Maj6
+	case Min7
+	case Maj7
+	case Octave
+	case Min9
+	case Maj9
+	case Min10
+	case Maj10
+	case Perfect11
+	case Aug11
+	case Perfect12
+	case Min13
+	case Maj13
+	case Min14
+	case Maj14
+	case Perfect15
+}
+
+public enum AugInterval: Int {
+	case Dim2 = 0
+	case AugUnison
+	case Dim3
+	case Aug2
+	case Dim4
+	case Aug3
+	case Dim5
+	case Dim6
+	case Aug5
+	case Dim7
+	case Aug6
+	case DimOctave
+	case Aug7 /* TODO: Also dim9?? */
+	case AugOctave
+	case Dim10
+	case Aug9
+	case Dim11
+	case Aug10
+	case Dim12
+	case Dim13
+	case Aug12
+	case Dim14
+	case Aug13
+	case Dim15
+	case Aug14
+	case Aug15
+}

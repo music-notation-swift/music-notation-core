2c299deca5285f811489c592111f370004b773aa
Kyle Sherman <kyledsherman@gmail.com>
2016-08-06 14:46:07 -0700

    Created files

---
+++ MusicNotationKit/MusicNotationKit/MeasureDurationValidator.swift
@@ -0,0 +1,11 @@
+//
+//  MeasureDurationValidator.swift
+//  MusicNotationKit
+//
+//  Created by Kyle Sherman on 8/6/16.
+//  Copyright © 2016 Kyle Sherman. All rights reserved.
+//
+
+public enum MeasureDurationValidator {
+    
+}
---
+++ MusicNotationKit/MusicNotationKitTests/MeasureDurationValidatorTests.swift
@@ -0,0 +1,14 @@
+//
+//  MeasureDurationValidatorTests.swift
+//  MusicNotationKit
+//
+//  Created by Kyle Sherman on 8/6/16.
+//  Copyright © 2016 Kyle Sherman. All rights reserved.
+//
+
+import XCTest
+
+class MeasureDurationValidatorTests: XCTestCase {
+
+    
+}

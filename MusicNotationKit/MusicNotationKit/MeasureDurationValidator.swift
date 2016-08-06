4fdf9e5b04e9ee0eec7e251e12882984fd8afb6e
Kyle Sherman <kyledsherman@gmail.com>
2016-08-06 16:56:10 -0700

    Made it compile

--- MusicNotationKit/MusicNotationKit/MeasureDurationValidator.swift
+++ MusicNotationKit/MusicNotationKit/MeasureDurationValidator.swift
@@ -7,5 +7,30 @@
 //
 
 public enum MeasureDurationValidator {
-    
+
+    public enum CompletionState {
+        case notFull
+        case full
+        case overfilled
+    }
+
+    public static func completionState(of measure: Measure) -> CompletionState {
+        // TODO: Implement
+        return .notFull
+    }
+
+    public static func number(of noteDuration: NoteDuration, fittingIn: Measure) -> Int {
+        // TODO: Implement
+        return 0
+    }
+
+    public static func overflowingNotes(for measure: Measure) -> Range<Int>? {
+        // TODO: Implement
+        return nil
+    }
+
+    public static func availableNotes(for measure: Measure) -> [NoteDuration : Int] {
+        // TODO: Implement
+        return [:]
+    }
 }
--- MusicNotationKit/MusicNotationKitTests/MeasureDurationValidatorTests.swift
+++ MusicNotationKit/MusicNotationKitTests/MeasureDurationValidatorTests.swift
@@ -7,8 +7,16 @@
 //
 
 import XCTest
+@testable import MusicNotationKit
 
 class MeasureDurationValidatorTests: XCTestCase {
 
-    
+    var fullMeasure: Measure!
+    var notFullMeasure: Measure!
+    var overfilledMeasure: Measure!
+
+    override func setUp() {
+        super.setUp()
+        // Create measures in valid in each state
+    }
 }

af33726813f4589f4ceb4b0bade604d509c421f3
Kyle Sherman <kyledsherman@gmail.com>
2016-09-11 19:06:35 -0700

    Added documentation

--- MusicNotationKit/MusicNotationKit/MeasureDurationValidator.swift
+++ MusicNotationKit/MusicNotationKit/MeasureDurationValidator.swift
@@ -6,8 +6,26 @@
 //  Copyright © 2016 Kyle Sherman. All rights reserved.
 //
 
+/**
+ This is a collection of static functions that will give information about the completeness of the duration of a 
+ `Measure`. A measure must have a certain number of notes according to its `TimeSignature` in order to be valid.
+ `MeasureDurationValidator` provides information as to the validitiy of the measure and any information that can be used
+ in order to be able to modify the measure so that it can be made valid.
+ */
 public enum MeasureDurationValidator {
 
+    /**
+     This represents the state of a measure's duration. It represents whether the `Measure` is full, notFull,
+     overfilled, or somehow invalid. The fullness is dictated by the time signature and how many notes are in the
+     measure when it is checked.
+     
+     - notFull: The measure doesn't have all the notes needed to be full. The `availableNotes` associated value will give a
+        dictionary of notes that will fit. It gives the smallest number of notes possible to fill.
+     - full: The measure is complete and cannot hold any more or less notes.
+     - overfilled: The measure has too many notes to be complete. The `overflowingNotes` associated value will give the range
+     of notes that would need to be removed for the measure to not be full anymore. Note that if the range of notes
+     is removed, it could be `full` or `notFull` depending on how large the last note removed was.
+     */
     public enum CompletionState: Equatable {
         case notFull(availableNotes: [NoteDuration : Int])
         case full
@@ -32,6 +50,9 @@ public enum MeasureDurationValidator {
 
     /**
      For the given measure, returns an array of `CompletionState` for each set in the measure in order.
+     
+     - parameter measure: The measure for which the `CompletionState` should be calculated.
+     - returns: The `CompletionState` for each set of notes in the measure in order.
      */
     public static func completionState(of measure: ImmutableMeasure) -> [CompletionState] {
         let baseDuration: NoteDuration
@@ -72,6 +93,57 @@ public enum MeasureDurationValidator {
         }
     }
 
+    /**
+     Returns the number of a certain `NoteDuration` that will fit in a measure in the specified note set.
+     
+     - parameter noteDuration: The note duration to check
+     - parameter measure: The measure to check
+     - parameter setIndex: The index of the note set to check. Default is 0.
+     - returns: The number of the specified duration that will fit in the measure within the specificed note set.
+     */
+    public static func number(of noteDuration: NoteDuration, fittingIn measure: ImmutableMeasure, inSet setIndex: Int = 0) -> Int {
+        let baseDuration: NoteDuration
+        do {
+            baseDuration = try baseNoteDuration(from: measure)
+        } catch {
+            // TODO: Write TimeSignature validation, so this isn't possible
+            return 0
+        }
+        let fullMeasureTicksBudget = measure.timeSignature.topNumber * baseDuration.ticks
+        let alreadyFilledTicks = measure.notes[setIndex].reduce(0) { prev, currentCollection in
+            return prev + currentCollection.noteTimingCount * currentCollection.noteDuration.ticks
+        }
+        let availableTicks = fullMeasureTicksBudget - alreadyFilledTicks
+        guard availableTicks > 0 else {
+            return 0
+        }
+        return availableTicks / noteDuration.ticks
+    }
+
+    /**
+     Calculates the `NoteDuration` that is associated with the bottom number of a `TimeSignature`. 
+     This handles irrational time signatures.
+     
+     - parameter measure: The measure for which the base duration should be derived.
+     - returns: The duration that is equivalent to the bottom number of the time signature associated with the specified
+        measure.
+     - throws:
+        - MeasureDurationValidatorError.invalidBottomNumber
+     */
+    internal static func baseNoteDuration(from measure: ImmutableMeasure) throws -> NoteDuration {
+        let bottomNumber = measure.timeSignature.bottomNumber
+        let rationalizedBottomNumber = Int(pow(2, floor(log(Double(bottomNumber)) / log(2))))
+
+        // TODO: (Kyle) We should validate in TimeSignature to make sure the number
+        // isn't too large. Then I guess we can make this a force unwrap, because the math above
+        // means it will always be a power of 2 and NoteDuration is always power of 2.
+        if let timeSignatureValue = NoteDuration.TimeSignatureValue(rawValue: rationalizedBottomNumber) {
+            return NoteDuration(timeSignatureValue: timeSignatureValue)
+        } else {
+            throw MeasureDurationValidatorError.invalidBottomNumber
+        }
+    }
+
     private static func availableNotes(within ticks: Int) -> [NoteDuration: Int] {
         var ticksLeft = ticks
         var availableNotes: [NoteDuration: Int] = [:]
@@ -102,39 +174,6 @@ public enum MeasureDurationValidator {
             }
         }
         return findLargest(start: 0, end: allTicks.count - 1)
-    }
-
-    public static func number(of noteDuration: NoteDuration, fittingIn measure: ImmutableMeasure, inSet setIndex: Int = 0) -> Int {
-        let baseDuration: NoteDuration
-        do {
-            baseDuration = try baseNoteDuration(from: measure)
-        } catch {
-            // TODO: Write TimeSignature validation, so this isn't possible
-            return 0
-        }
-        let fullMeasureTicksBudget = measure.timeSignature.topNumber * baseDuration.ticks
-        let alreadyFilledTicks = measure.notes[setIndex].reduce(0) { prev, currentCollection in
-            return prev + currentCollection.noteTimingCount * currentCollection.noteDuration.ticks
-        }
-        let availableTicks = fullMeasureTicksBudget - alreadyFilledTicks
-        guard availableTicks > 0 else {
-            return 0
-        }
-        return availableTicks / noteDuration.ticks
-    }
-
-    internal static func baseNoteDuration(from measure: ImmutableMeasure) throws -> NoteDuration {
-        let bottomNumber = measure.timeSignature.bottomNumber
-        let rationalizedBottomNumber = Int(pow(2, floor(log(Double(bottomNumber)) / log(2))))
-
-        // TODO: (Kyle) We should validate in TimeSignature to make sure the number
-        // isn't too large. Then I guess we can make this a force unwrap, because the math above 
-        // means it will always be a power of 2 and NoteDuration is always power of 2.
-        if let timeSignatureValue = NoteDuration.TimeSignatureValue(rawValue: rationalizedBottomNumber) {
-            return NoteDuration(timeSignatureValue: timeSignatureValue)
-        } else {
-            throw MeasureDurationValidatorError.invalidBottomNumber
-        }
     }
 }
 

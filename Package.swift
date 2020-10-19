5d1529199959e5739cf37f5965a2372c532abe2b
woolie <steven_woolgar@woolsoft.com>
2020-10-19 10:09:47 -0700

    Futzing with the path

--- Package.swift
+++ Package.swift
@@ -3,18 +3,17 @@ import Foundation
 import PackageDescription
 
 var package = Package(
-	name: "swift-MusicNotationCore",
+	name: "MusicNotationCore",
 	products: [
 		.library(name: "MusicNotationCoreMac", targets: ["MusicNotationCoreMac"]),
 		.library(name: "MusicNotationCoreiOS", targets: ["MusicNotationCoreiOS"]),
 		.library(name: "MusicNotationCoreTV", targets: ["MusicNotationCoreTV"]),
 	],
 	targets: [
-		.target(name: "MusicNotationCoreMac", dependencies: []),
+		.target(name: "MusicNotationCoreMac", path: "MusicNotationCore"),
 		.testTarget(name: "MusicNotationCoreMacTests", dependencies: ["MusicNotationCoreMac"]),
-
-		.target(name: "MusicNotationCoreiOS"),
-		.target(name: "MusicNotationCoreTV"),
+		.target(name: "MusicNotationCoreiOS", path: "MusicNotationCore"),
+		.target(name: "MusicNotationCoreTV", path: "MusicNotationCore"),
 	],
 	swiftLanguageVersions: [
 		.v5

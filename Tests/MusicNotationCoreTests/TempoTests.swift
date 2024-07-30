28a223d4d306d091a935c354e967dbbeb61bbbcc
Steven Woolgar <steven_woolgar@woolsoft.com>
2024-07-30 10:35:55 -0700

    Equality tests

--- Tests/MusicNotationCoreTests/TempoTests.swift
+++ Tests/MusicNotationCoreTests/TempoTests.swift
@@ -19,4 +19,20 @@ import Testing
         let tempo = Tempo(type: .linear, position: 10.3, value: 120, unit: .quarter, text: "Intro")
         #expect(tempo.debugDescription == "type: linear, position: 10.3, value: 120, unit: quarter, text: Intro")
     }
+
+    @Test func tempoEqualityCheck() async throws {
+        let tempo = Tempo(type: .linear, position: 10.3, value: 120, unit: .quarter, text: "Intro")
+        #expect(tempo == Tempo(type: .linear, position: 10.3, value: 120, unit: .quarter, text: "Intro"))
+    }
+
+    @Test func tempoInequalityCheck() async throws {
+        let tempo = Tempo(type: .linear, position: 10.3, value: 120, unit: .quarter, text: "Intro")
+        #expect(tempo != Tempo(type: .linear, position: 12.3, value: 120, unit: .quarter, text: "Intro"))
+        #expect(tempo != Tempo(type: .undefined, position: 10.3, value: 120, unit: .quarter, text: "Intro"))
+        #expect(tempo != Tempo(type: .pause, position: 10.3, value: 120, unit: .quarter, text: "Intro"))
+        #expect(tempo != Tempo(type: .ramp, position: 10.3, value: 120, unit: .quarter, text: "Intro"))
+        #expect(tempo != Tempo(type: .linear, position: 10.3, value: 110, unit: .quarter, text: "Intro"))
+        #expect(tempo != Tempo(type: .linear, position: 10.3, value: 120, unit: .dottedHalf, text: "Intro"))
+        #expect(tempo != Tempo(type: .linear, position: 10.3, value: 120, unit: .quarter))
+    }
 }
